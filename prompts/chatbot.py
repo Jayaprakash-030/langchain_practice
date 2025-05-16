97cfdfa2f07f52a4c11cb67215b765a95fe0cc5a
Jayaprakash <jayprakeshsai@gmail.com>
2025-05-16 02:01:21 +0000

    Completed prompts class

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -1,29 +1,50 @@
-from langchain_huggingface import HuggingFaceEndpoint, HuggingFacePipeline
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from dotenv import load_dotenv
-import warnings
+from huggingface_hub import login
 
-warnings.filterwarnings("ignore")
-load_dotenv()
+import os
+import torch
 
+load_dotenv(dotenv_path="../.env")
 
-llm = HuggingFaceEndpoint(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-    task="text-generation",
-    temperature=0.2,
-    max_new_tokens=64,
-    do_sample=True,
-    top_p=0.95,
-    return_full_text=False,
+token = os.getenv("HF_TOKEN")
+login(token=token)
+
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    device_map="auto",
 )
 
-def format_prompt(user_input):
-    return f"### Instruction:\n{user_input}\n\n### Response:"
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    temperature=0.2,
+    max_new_tokens=64,
+    top_p=0.95,
+    do_sample=True,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+llm = HuggingFacePipeline(pipeline=pipe)
+
+chat_history = [
+    SystemMessage(content="You are helpful AI assistant")
+]
 
 while True:
     user_input = input("You: ")
+    chat_history.append(HumanMessage(content=user_input))
     if user_input.lower() == "exit":
         break
-    else:
-        prompt = format_prompt(user_input=user_input)
-        result = llm.invoke(prompt)
-        print("AI:", result)
+    result = llm.invoke(chat_history)
+    print("AI: ", result)
+    chat_history.append(AIMessage(content=result))
+
+print(chat_history)
