bfb9b38ec1f8ecb92501678f7b4798597516f4bf
Jayaprakash <jayprakeshsai@gmail.com>
2025-05-18 00:43:16 +0000

    Corrected pydantic code flow, and added simple chain file

--- output_parsers/pydantic_output_parser.py
+++ output_parsers/pydantic_output_parser.py
@@ -1,15 +1,38 @@
 from langchain_huggingface import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import PydanticOutputParser
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from huggingface_hub import login
 import os
+import torch
 
-load_dotenv(dotenv_path="../.env")
-token = os.getenv("HF_TOKEN")
-login(token=token)
+# load_dotenv(dotenv_path="../.env")
+# token = os.getenv("HF_TOKEN")
+# login(token=token)
 
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    temperature=0.2,
+    do_sample=True,
+    max_new_tokens=256,
+    return_full_text=False,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+llm = HuggingFacePipeline(pipeline=pipe)
 
 class Person(BaseModel):
     name: str = Field(description="Name of the person")
@@ -19,31 +42,13 @@ class Person(BaseModel):
 parser = PydanticOutputParser(pydantic_object=Person)
 
 template = PromptTemplate(
-    template=(
-        "Generate a fictional {place} person.\n"
-        "Return only a JSON object"
-        "{format_instruction}"
-    ),
+    template="Generate a fictional {place} person.\n {format_instruction}",
     input_variables=["place"],
-    partial_variables={"format_instruction": parser.get_format_instructions()},
-)
-
-llm = HuggingFacePipeline.from_model_id(
-    model_id="meta-llama/Llama-2-7b-chat-hf",
-    task="text-generation",
-    device=0,
-    model_kwargs={
-        "torch_dtype": "auto"
-    },
-    pipeline_kwargs={
-        "temperature": 0.7,
-        "pad_token_id": 128001,
-        "do_sample": True,
-        "return_full_text": False
-    }
+    partial_variables={"format_instruction": parser.get_format_instructions()}
 )
 
 chain = template | llm | parser
 
 result = chain.invoke({"place": "Indian"})
 print(result)
+print(type(result))
