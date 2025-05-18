faa891ccafa03bf26f9c6835a286b9a0966d7f30
Jayaprakash <jayprakeshsai@gmail.com>
2025-05-18 00:46:44 +0000

    Corrected structured output parser file

--- output_parsers/structured_json_output_parser.py
+++ output_parsers/structured_json_output_parser.py
@@ -1,30 +1,34 @@
 from langchain_huggingface import HuggingFacePipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain.prompts import PromptTemplate
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
-from huggingface_hub import login
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+from huggingface_hub import login
 import os
 import torch
 
-load_dotenv(dotenv_path="../.env")
-token = os.getenv("HF_TOKEN")
-login(token=token)
+# load_dotenv(dotenv_path="../.env")
+# token = os.getenv("HF_TOKEN")
+# login(token=token)
 
-model_id = "meta-llama/Llama-2-7b-chat-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=token)
+model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     torch_dtype=torch.float16,
     device_map="auto",
-    use_auth_token=token
 )
 
 pipe = pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
-    temperature=0.1,
+    temperature=0.2,
+    do_sample=True,
+    max_new_tokens=256,
+    return_full_text=False,
     pad_token_id=tokenizer.eos_token_id
 )
 
@@ -43,7 +47,7 @@ prompt = PromptTemplate(
     partial_variables={"format_instruction": parser.get_format_instructions()},
 )
 
-chain = prompt | llm
+chain = prompt | llm | parser
 
 result = chain.invoke({"topic": "cricket"})
 print(result)
